23ffc63d80da3ff6dfe4e764974b14fdd058629d
celine-yes <celine21106@gmail.com>
2023-03-17 19:04:06 +0100

    marche pas

---
+++ TME6_7/README.md
@@ -0,0 +1,19 @@
+# TME 6-7
+
+## 1. MISE EN OEUVRE AVEC UN TAMPON A UNE SEULE CASE
+
+#### 1.1
+
+Il faut créer un autre sémaphore pour garantir l'exclusion mutuelle de nb_recepteurs.
+
+Les initialisations des sémaphores et des variables partagées sont les suivantes :
+
+Initialisation du sémaphore EMET à 1, ce qui permettra aux émetteurs de commencer à écrire immédiatement s'il y a une case vide dans le tampon.
+Initialisation des sémaphores du tableau RECEP à 0 pour bloquer tous les récepteurs jusqu'à ce qu'il y ait un message à lire.
+Initialisation du compteur partagé nb_recepteurs à 0 pour indiquer que aucun récepteur n'a encore lu le premier message produit.
+
+#### 1.2
+
+On utilise un tableau de sémaphore RECEP[1..NR], dont chaque case est augmentée de 1 lors de l'émission d'un message plutôt qu'un unique sémaphore RECEP pour éviter de bloquer tous les récepteurs quand un récepteur n'a plus rien à lire, en effet cela permet à plusieurs recepteurs de travailler en parallèle.
+
+Les émetteurs sont prévenus de la disponibilité de la case lorsque tous les recepteurs  ont lu le message dans la case, donc si nb_recepteurs = NR.    
---
+++ TME6_7/libipc/Tab1case.c
@@ -0,0 +1,164 @@
+/* Diffusion tampon 1 case */
+
+  #include <stdio.h>
+  #include <unistd.h>
+  #include <stdlib.h>
+  #include <signal.h>
+  #include <libipc.h>
+
+/************************************************************/
+
+/* definition des parametres */
+
+  #define NE          2     /*  Nombre d'emetteurs         */
+  #define NR          5     /*  Nombre de recepteurs       */
+
+/************************************************************/
+
+/* definition des semaphores */
+  #define SEMNR      0
+	#define EMET       1
+  #define RECEP      2
+
+
+/************************************************************/
+
+/* definition de la memoire partagee */
+
+  typedef struct {
+    int mess;
+    int nb_recepteurs;
+  } t_segpart;
+
+  t_segpart *sp;
+
+/************************************************************/
+
+/* variables globales */
+  int emet_pid[NE], recep_pid[NR];
+
+/************************************************************/
+
+/* traitement de Ctrl-C */
+
+  void handle_sigint(int sig) {
+      int i;
+      for (i = 0; i < NE; i++) kill(emet_pid[i], SIGKILL);
+      for (i = 0; i < NR; i++) kill(recep_pid[i], SIGKILL);
+      det_sem();
+      det_shm((char *)sp);
+  }
+
+/************************************************************/
+
+/* fonction EMETTEUR */
+
+void emetteur(int mess){
+  while(1){
+    P(EMET);
+    printf("aaaaaaaaaaaaaaaaaaaaa\n");
+
+    sp->nb_recepteurs = 0;
+    sp->mess = mess;
+    V(RECEP);
+  }
+}
+
+/************************************************************/
+
+/* fonction RECEPTEUR */
+
+  void recepteur(int id){
+  int m;
+  while(1){
+    printf("ooooooooooouuuuuuuuuuuuuuuu\n");
+    P(RECEP + id);
+    printf("ooooooooooo\n");
+    m = sp->mess;
+    printf("Recepteur %d a lu la valeur %d\n", getpid(), m);
+    P(SEMNR);
+    sp->nb_recepteurs++;
+    V(SEMNR);
+    if(sp->nb_recepteurs == NR){
+      printf("bbbbbbbbbbbbbbb\n");
+      V(EMET);
+    }
+  }
+  }
+
+/************************************************************/
+
+int main() {
+  struct sigaction action;
+  /* autres variables (a completer) */
+  int semid, pid;
+  setbuf(stdout, NULL);
+
+/* Creation du segment de memoire partagee */
+
+  if ( (sp = (t_segpart *)init_shm(sizeof(t_segpart) ))== NULL) {
+    perror("init_shm");
+    exit(1);
+  }
+
+/* creation des semaphores */
+
+  if ((semid = creer_sem(NR + 2)) == -1) {
+      perror("creer_sem");
+      exit(1);
+  }
+
+/* initialisation des semaphores */
+  init_un_sem(SEMNR, 1);
+  init_un_sem(EMET, 1);
+  for (int i = 0; i < NR; i++){
+    init_un_sem(RECEP+i, 0);
+  }
+
+
+/* creation des processus emetteurs */
+  for (int i = 0; i < NE; i++){
+    pid = fork();
+    if (pid == -1){
+      perror("fork");
+      exit(1);
+    } else if (pid == 0){
+        /* processus fils */
+        emetteur(i+1);
+        printf("iciiiiiiiiiiii\n");
+        exit(EXIT_SUCCESS);
+    } else {
+        /* processus pere */
+        emet_pid[i] = pid;
+    }
+  }
+
+
+/* creation des processus recepteurs */
+
+  for (int i = 0; i < NR; i++){
+    pid = fork();
+    if (pid == -1){
+      perror("fork");
+      exit(1);
+    } else if (pid == 0){
+        /* processus fils */
+        recepteur(i+1);
+        exit(EXIT_SUCCESS);
+    } else {
+        /* processus pere */
+        recep_pid[i] = pid;
+    }
+  }
+
+
+/* redefinition du traitement de Ctrl-C pour arreter le programme */
+
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    action.sa_handler = handle_sigint;
+    sigaction(SIGINT, &action, 0);
+
+    pause();                    /* attente du Ctrl-C  */
+    return EXIT_SUCCESS;
+}
